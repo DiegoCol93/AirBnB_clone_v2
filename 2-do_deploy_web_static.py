7a053a54a2e3d34c61b73a0898a576c75dff3278
DiegoCol93 <dfelo93@gmail.com>
2021-04-15 13:12:54 -0500

    |  ADDED  | try except for treturning false on error.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,17 +11,20 @@ def do_deploy(archive_path):
 
     if archive_path is None or exists(archive_path) is False:
         return(False)
-    archive_name = archive_path.split("/")[1].split(".")[0]
-
-    put(archive_path, "/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(archive_name))
-    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
-        .format(archive_name, archive_name))
-    run("rm /tmp/{}.tgz".format(archive_name))
-    run("mv /data/web_static/releases/{}/web_static/* "
-        "/data/web_static/releases/{}/"
-        .format(archive_name, archive_name))
-    run("rm -rf /data/web_static/releases/{}/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{} /data/web_static/current"
-        .format(archive_name, archive_name))
+    try:
+        archive_name = archive_path.split("/")[1].split(".")[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}".format(archive_name))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
+            .format(archive_name, archive_name))
+        run("rm /tmp/{}.tgz".format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/"
+            .format(archive_name, archive_name))
+        run("rm -rf /data/web_static/releases/{}/web_static")
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(archive_name, archive_name))
+        return (True)
+    except Exception as e:
+        return (False)
