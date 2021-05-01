d6c966d043a23538b137f5cf5d48941200a124c6
DiegoCol93 <dfelo93@gmail.com>
2021-05-01 10:08:43 -0500

    |- ADDED -| /hbnb route for Flask server.

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Sets a basic flask server.
+""" Module for storing the Flask setup. """
+from flask import Flask, escape, request
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBNB():
+    """ Returns the content at Flask's root."""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hello_HBNB():
+    """ Returns the content at Flask's route /hbnb."""
+    return ("HBNB")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
