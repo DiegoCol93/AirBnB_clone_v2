b999a7176b4e56b88abf0297c070ad4a7b03e018
DiegoCol93 <dfelo93@gmail.com>
2021-05-01 11:16:45 -0500

    |- ADDED -| /python route.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# Sets a basic flask server.
+""" Module for storing the Flask setup. """
+from flask import Flask, escape, request
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+# Root route. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+@app.route('/')
+def hello_HBNB():
+    """ Returns the content at Flask's root."""
+    return ("Hello HBNB!")
+
+
+# /hbnb route. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
+@app.route('/hbnb/')
+def HBNB():
+    """ Returns the content at Flask's route /hbnb."""
+    return ("HBNB")
+
+
+# /c/<text> route. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
+@app.route('/c/<text>')
+def text_route(text):
+    """ Returns the content at Flask's route /hbnb."""
+    text = text.replace("_", " ")
+    return ("C " + text)
+
+
+# /c/<text> route. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_route(text):
+    """ Returns the content at Flask's route /hbnb."""
+    text = text.replace("_", " ")
+    return ("Python " + text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
