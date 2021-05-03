cecd254fe1daa3aa3ab8ae0261f3e777ffc355bc
DiegoCol93 <dfelo93@gmail.com>
2021-05-02 20:08:55 -0500

    |- ADDED -| route for HBNB /states_list.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Sets a basic flask server.
+""" Module for storing the Flask setup. """
+from flask import Flask, render_template
+from models.__init__ import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+# State List route. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+@app.route('/states_list')
+def states_list():
+    """ Returns the list of states currently on the HBNB_storage. """
+
+    # Bring dictionary from storage.
+    models = storage.all(State)
+    template = '7-states_list.html'
+
+    # Return with the jinja template.
+    return render_template(template, models=models)
+
+
+# Close ORM connection. - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+@app.teardown_appcontext
+def teardown_db_connection(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
